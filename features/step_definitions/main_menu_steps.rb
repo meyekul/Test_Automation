bbcf9d29bac9ea762b8361da3fa94b4c428cac8b
Michael Doyle <meyekkul@gmail.com>
2019-05-20 18:29:11 +0800

    adding initial automation scenarios

---
+++ features/step_definitions/main_menu_steps.rb
@@ -0,0 +1,20 @@
+Given("I land on home screen") do
+  puts("Landed on home screen")
+end
+
+When("I press on menu icon") do
+  puts("Menu icon pressed")
+end
+
+Then("I should see left side menu") do
+  puts("I see left side menu")
+end
+
+
+Then("I press on My conversions button") do
+  puts ("I land on my conversions screen")
+end
+
+Then("I land on My conversions screen") do
+  puts("I see empty list")
+end
