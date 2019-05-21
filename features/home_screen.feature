ca0a0442509e00a4ed61ee6bdbc52e95a673ed41
Michael Doyle <meyekkul@gmail.com>
2019-05-21 08:58:59 +0800

    updating features and step definitions

--- features/home_screen.feature
+++ features/home_screen.feature
@@ -5,6 +5,26 @@ Feature: Tests for home screen functionality
     Then Left unit picker should be "inches"
     And Right unit picker should be "Centimetre"
 
+  Scenario: Show All button shuold be enabled at launch
+    Given I land on home screen
+    Then Show All button should be enabled
+    When I press on Clear button
+    Then Show All button should be enabled
+    Then Show All button should be undefined
+
+    @WIP
+  Scenario: Verify default conversion
+    Given I land on home screen
+    When I type "1" to target text field
+    Then I should see the result as "12"
+
+
+
+
+
+
+
+
 
 
 
--- features/step_definitions/home_screen_steps.rb
+++ features/step_definitions/home_screen_steps.rb
@@ -8,15 +8,28 @@ Then("Right unit picker should be {string}") do |value|
 end
 
 
+Then(/^Show All button should be (enabled|disabled|undefined)$/) do |state|
+  if state == "enabled"
+    puts("button is enabled")
+    elsif state == "disabled"
+    puts("button is disabled")
+  elsif state == "undefined"
+    puts("button state is undefined")
+  end
+end
 
 
+When("I press on Clear button") do
+  puts("Clear button is pressed")
+end
 
 
+When("I type {string} to target text field") do |string|
+  puts("target is #{string}")
+end
 
 
-
-
-
-
-
+Then("I should see the result as {string}") do |result|
+  puts("result is #{result}")
+end
 
